9d94653d4d1381eedab42f4b5c531a8882cfa7a0
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2020-09-30 20:06:50 +0200

    updated and commented data containers examples

--- src/main/java/org/matsim/matsim_api_examples/DataContainersExamples.java
+++ src/main/java/org/matsim/matsim_api_examples/DataContainersExamples.java
@@ -1,16 +1,14 @@
 package org.matsim.matsim_api_examples;
 
-import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.network.NetworkFactory;
 import org.matsim.api.core.v01.network.Node;
-import org.matsim.api.core.v01.population.*;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.ControlerUtils;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.network.io.NetworkWriter;
@@ -26,107 +24,120 @@ import org.matsim.pt.transitSchedule.TransitScheduleFactoryImpl;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
-
-import java.util.Arrays;
-import java.util.HashSet;
+import org.matsim.vehicles.MatsimVehicleReader;
+import org.matsim.vehicles.MatsimVehicleWriter;
+import org.matsim.vehicles.VehicleUtils;
+import org.matsim.vehicles.Vehicles;
 
 public class DataContainersExamples {
 
     public static void main(String[] args) {
 
-        // Create the data containers using the scenario
+        // There are two ways to create EMPTY data containers
+
+        // 1) Create the empty data containers using the scenario
+
+        // create an empty scenario
         Config config = ConfigUtils.createConfig();
         Scenario scenario = ScenarioUtils.createScenario(config);
-        Network network = scenario.getNetwork();
+
+        // create the empty data containers
+        System.out.println("Creating empty data containers using scenario.");
+        Network emptyNetworkFromScenario = scenario.getNetwork();
+        Population emptyPopulationFromScenario = scenario.getPopulation();
+        ActivityFacilities emptyFacilitiesFromScenario = scenario.getActivityFacilities();
+        Vehicles emptyVehiclesFromScenario = scenario.getVehicles();
+        TransitSchedule emptyTransitScheduleFromScenario = scenario.getTransitSchedule();
+
+        // We will prove here that these data containers are in fact empty
+        System.out.println("Number of links in empty network: " + emptyNetworkFromScenario.getLinks().size());
+        System.out.println("Number of persons in empty population: " + emptyPopulationFromScenario.getPersons().size());
+        System.out.println("Number of facilities in empty facilities container: " + emptyFacilitiesFromScenario.getFacilities().size());
+        System.out.println("Number of vehicles in empty vehicles container: " + emptyVehiclesFromScenario.getVehicles().size());
+        System.out.println("Number of transit lines in empty transit schedule: " + emptyTransitScheduleFromScenario.getTransitLines().size());
+
+        // 2) Create them using *Utils-classes
+        System.out.println("Creating empty data containers using *Utils-classes.");
+        Network emptyNetworkFromUtils = NetworkUtils.createNetwork();
+        Population emptyPopulationFromUtils = PopulationUtils.createPopulation(config);
+        ActivityFacilities emptyFacilitiesFromUtils = FacilitiesUtils.createActivityFacilities();
+        Vehicles emptyVehiclesFromUtils = VehicleUtils.createVehiclesContainer();
+        TransitSchedule emptyTransitScheduleFromUtils = new TransitScheduleFactoryImpl().createTransitSchedule();  // exception from the *Utils-pattern
+
+        // Again, we will prove here that these data containers are in fact empty
+        System.out.println("Number of links in empty network: " + emptyNetworkFromUtils.getLinks().size());
+        System.out.println("Number of persons in empty population: " + emptyPopulationFromUtils.getPersons().size());
+        System.out.println("Number of facilities in empty facilities container: " + emptyFacilitiesFromUtils.getFacilities().size());
+        System.out.println("Number of vehicles in empty vehicles container: " + emptyVehiclesFromUtils.getVehicles().size());
+        System.out.println("Number of transit lines in empty transit schedule: " + emptyTransitScheduleFromUtils.getTransitLines().size());
+
+
+        // These various Utils classes also contain many helper functions
+        // We suggest you look through them to see what is available
+
+
+        // Otherwise, we can load the different data containers from file
+        // For each data container, the exact logic for doing this is slightly different
+
+        // Network
+        // 1) create empty network
+        Network network = NetworkUtils.createNetwork();
+        // 2) pass it to network reader and then read in the file
+        new MatsimNetworkReader(network).readFile("scenarios/siouxfalls-2014/Siouxfalls_network_PT.xml");
+
+        // Population
+        // 1) create an empty (we can use the one we created previously)
+        // 2) pass the scenario to population reader and then read in the file
+        new PopulationReader(scenario).readFile("scenarios/siouxfalls-2014/Siouxfalls_population.xml.gz");
+        // 3) extract the population from the scenario
         Population population = scenario.getPopulation();
+
+        // Facilities (same logic as population)
+        new MatsimFacilitiesReader(scenario).readFile("scenarios/siouxfalls-2014/Siouxfalls_facilities.xml.gz");
         ActivityFacilities facilities = scenario.getActivityFacilities();
+
+        // TransitSchedule (same logic as population)
+        new TransitScheduleReader(scenario).readFile("scenarios/siouxfalls-2014/Siouxfalls_transitSchedule.xml");
         TransitSchedule transitSchedule = scenario.getTransitSchedule();
 
-        // or using *Utils-classes to create them
-        Network network2 = NetworkUtils.createNetwork();
-        Population population2 = PopulationUtils.createPopulation(config);
-        ActivityFacilities facilities2 = FacilitiesUtils.createActivityFacilities();
-        TransitSchedule schedule = new TransitScheduleFactoryImpl().createTransitSchedule();  // exception from the *Utils-pattern
+        // Vehicles (same logic as network, more or less)
+        Vehicles vehicles = VehicleUtils.createVehiclesContainer();
+        new MatsimVehicleReader.VehicleReader(vehicles).readFile("scenarios/siouxfalls-2014/Siouxfalls_vehicles.xml");
 
-        // read in data containers
-        new MatsimNetworkReader(network).readFile("/path/to/input/network.xml");
-        new PopulationReader(scenario).readFile("/path/to/input/population.xml");
-        new MatsimFacilitiesReader(scenario).readFile("/path/to/input/facilities.xml");
-        new TransitScheduleReader(scenario).readFile("/path/to/input/transitSchedule.xml");
+        // Now, we will prove here that these data containers are NOT empty!
+        System.out.println("Number of links in loaded network: " + network.getLinks().size());
+        System.out.println("Number of persons in loaded population: " + population.getPersons().size());
+        System.out.println("Number of facilities in loaded facilities container: " + facilities.getFacilities().size());
+        System.out.println("Number of vehicles in loaded vehicles container: " + vehicles.getVehicles().size());
+        System.out.println("Number of transit lines in loaded transit schedule: " + transitSchedule.getTransitLines().size());
 
-        // write out the data to xml
-        new NetworkWriter(network).write("/path/to/output/network.xml");
-        new PopulationWriter(population).write("/path/to/output/population.xml");
-        new FacilitiesWriter(facilities).write("/path/to/output/facilities.xml");
-        new TransitScheduleWriter(transitSchedule).writeFile("/path/to/output/transitSchedule.xml");
+
+        // We can also write these containers out to file
+        new NetworkWriter(network).write("output/network.xml");
+        new PopulationWriter(population).write("output/population.xml");
+        new FacilitiesWriter(facilities).write("output/facilities.xml");
+        new TransitScheduleWriter(transitSchedule).writeFile("output/transitSchedule.xml");
+        new MatsimVehicleWriter(vehicles).writeFile("output/transitSchedule.xml");
+
 
         // IDs
         // Elements in data containers have Ids for identification.
 
-        // These IDs have a type:
-        Id<Link> linkId;
-        Id<Node> nodeId;
-        Id<Person> personId;
-
-        // The type must be specified when creating an Id:
-        linkId = Id.create("link", Link.class);
-        nodeId = Id.create("node", Node.class);
-        personId = Id.create("person", Person.class);
+        // These IDs have a type which must be specified when creating an Id:
+        Id<Link> linkId1 = Id.create("link", Link.class);
+        Id<Node> nodeId1 = Id.create("node", Node.class);
+        Id<Person> personId1 = Id.create("person", Person.class);
 
         // Alternatively, some id type have specific creation methods
         Id<Link> linkId2 = Id.createLinkId("link2");
         Id<Node> nodeId2 = Id.createNodeId("node2");
         Id<Person> personId2 = Id.createPersonId("person2");
 
+
         // Factories
         // Use Factories to modify create new elements for data containers.
         // Each data container provides its own factory.
-
-        // Network example
-        NetworkFactory networkFactory = network.getFactory();
-        Node node1 = networkFactory.createNode(Id.createNodeId("1"), new Coord(0,0));
-        Node node2 = networkFactory.createNode(Id.createNodeId("2"), new Coord(10,10));
-
-        Link link = networkFactory.createLink(Id.createLinkId("1"), node1, node2);
-        link.setFreespeed(50.0 / 3.6); // 50 km/h in m/s
-        link.setLength(300); // meter
-        link.setCapacity(800); // veh / hour
-        link.setNumberOfLanes(1);
-        link.setAllowedModes(new HashSet<>(Arrays.asList("car", "bus")));
-
-        // Don't forget to add the created elements to the data container.
-        network.addNode(node1);
-        network.addNode(node2);
-        network.addLink(link);
-
-        // Population example
-        PopulationFactory populationFactory = population.getFactory();
-
-        // Create a person and its plan
-        Person person = populationFactory.createPerson(Id.createPersonId("person"));
-        Plan plan = populationFactory.createPlan();
-
-        // Create activities and legs
-        Activity act1 = populationFactory.createActivityFromCoord("home", new Coord(150, 250));
-        act1.setEndTime(8 * 3600);
-        Leg leg = populationFactory.createLeg("car");
-        Activity act2 = populationFactory.createActivityFromCoord("work", new Coord(750, 650));
-
-        // Add activities and legs to plan
-        plan.addActivity(act1);
-        plan.addLeg(leg);
-        plan.addActivity(act2);
-
-        // Add plan to person
-        person.addPlan(plan);
-
-        // Add person to population
-        population.addPerson(person);
-
-        // Attributes
-        // Attributes can be stored within the objects
-        person.getAttributes().putAttribute("age", 39);
-        int age = (Integer) person.getAttributes().getAttribute("age");
+        // More detailed use of these factories can be seen in the CreatePopulationExample or CreateNetworkExample
 
     }
 }
