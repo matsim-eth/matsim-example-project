61173b2eacd099230138af0e46dbc0954b84b2bc
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2020-09-30 19:40:55 +0200

    update create network example with comments

--- src/main/java/org/matsim/matsim_api_examples/CreateNetworkExample.java
+++ src/main/java/org/matsim/matsim_api_examples/CreateNetworkExample.java
@@ -2,11 +2,9 @@ package org.matsim.matsim_api_examples;
 
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.*;
-import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -15,33 +13,34 @@ public class CreateNetworkExample {
 
     public static void main(String[] args) {
 
-        // create network data container and factory
-        Config config = ConfigUtils.createConfig();
-        Scenario scenario = ScenarioUtils.createScenario(config);
-        Network network = scenario.getNetwork();
+        // Create empty network data container
+        Network network = NetworkUtils.createNetwork();
+
+        // Get the network factory
         NetworkFactory factory = network.getFactory();
 
-        // create nodes
-        Node node0 = factory.createNode(Id.createNodeId("0"), new Coord(0.0, 0.0));
-        Node node1 = factory.createNode(Id.createNodeId("1"), new Coord(1000.0, 0.0));
+        // Create nodes using the factory (notice we used different ways for creating the node ids and the coordinates)
+        Node node0 = factory.createNode(Id.create("0", Node.class), CoordUtils.createCoord(0.0, 0.0)); // first option
+        Node node1 = factory.createNode(Id.createNodeId("1"), new Coord(100.0, 100.0)); // second option
 
-        // add to network
-        network.addNode(node0);
-        network.addNode(node1);
-
-        // create links
+        // Create links using the factory
         Link link = factory.createLink(Id.createLinkId("0_1"), node0, node1);
-        link.setCapacity(1800.0);
-        link.setLength(1000.0);
-        link.setFreespeed(50.0 / 3.6);
+
+        // Set link attributes (otherwise they will be set to default values)
+        link.setFreespeed(50.0 / 3.6); // 50 km/h in m/s
+        link.setLength(300); // meter
+        link.setCapacity(800); // veh / hour
         link.setNumberOfLanes(1);
         link.setAllowedModes(new HashSet<>(Arrays.asList("car", "bus")));
 
-        // add to network
+        // Don't forget to add the created elements to the data container.
+        network.addNode(node0);
+        network.addNode(node1);
         network.addLink(link);
 
         // write network to file
-        new NetworkWriter(network).write("output/network.xml");
+        new NetworkWriter(network).write("output/simple_network_example.xml");
+
     }
 
 }
